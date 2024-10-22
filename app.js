57340234a8704ed301f4779d5d36f5f3c5e326a7
karl <karlkivisild@gmail.com>
2024-10-22 09:18:47 +0300

    Create error handling for unit and integration tests

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ app.use(express.json())
 
 app.use('/todos', todoRoutes)
 
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: error.message })
+})
+
 app.get('/', (req, res) => {
     res.send('express test')
 })
 
-// app.listen(3015, () => {
-//    console.log('server is running')
-// })
 
 module.exports = app
